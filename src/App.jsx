4668f59a768b0cf246a84e068559d4b6010c382b
jun-hwan00 <0528klkl@naver.com>
2025-09-13 17:56:32 +0900

    feat: create project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './styles/main.css'      
+import './styles/noscript.css'  
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello Tailwind!
+      </h1>
+      <p className="mt-4 text-gray-700">
+        Tailwind CSS is working!
+      </p>
+    </div>
+  )
+}
+
+export default App
